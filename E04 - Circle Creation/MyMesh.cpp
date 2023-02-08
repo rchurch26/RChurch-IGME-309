3231cb2b06bcfb2f6a576e8ebfd6e2201a8479f6
Rahmel Church Jr <rac3090@rit.edu>
2023-02-08 16:54:05 -0500

    Began E04

--- E04 - Circle Creation/MyMesh.cpp
+++ E04 - Circle Creation/MyMesh.cpp
@@ -16,9 +16,14 @@ void MyMesh::GenerateCircle(float a_fRadius, int a_nSubdivisions, vector3 a_v3Co
 		Calculate a_nSubdivisions number of points around a center point in a radial manner
 		then call the AddTri function to generate a_nSubdivision number of faces
 	*/
-	AddTri(	vector3(0.0f, 0.0f, 0.0f),
+	int rotations = 360 / a_nSubdivisions;
+	for (int i = 0; i < a_nSubdivisions; i++)
+	{
+
+		AddTri(vector3(glm::cos(rotations) * i, glm::sin(rotations) * i, 0.0f),
 			vector3(1.0f, 0.0f, 0.0f),
 			vector3(0.77f, 0.77f, 0.0f));
+	}
 
 	// Adding information about color
 	CompleteMesh(a_v3Color);
