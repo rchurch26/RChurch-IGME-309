c3a319b1654c93c5258dd770dad5fee21d79a55a
Rahmel Church Jr <rac3090@rit.edu>
2023-02-21 11:01:07 -0500

    More issues

--- HW02_3DPrimitives_Solution/MyMesh.cpp
+++ HW02_3DPrimitives_Solution/MyMesh.cpp
@@ -248,6 +248,10 @@ void MyMesh::GenerateTorus(float a_fOuterRadius, float a_fInnerRadius, int a_nSu
 	// Replace this with your code
 	//Create List of Verticies
 	std::vector<vector3> verticies;
+	//Create List of Circle Verticies
+	std::vector<vector3> circleVerticies;
+	//Create List of Quad Verticies
+	std::vector<vector3> quadVerticies;
 
 	//Grab Angle of Torus
 	float angle = PI * 2.0f / a_nSubdivisionsA;
@@ -259,13 +263,11 @@ void MyMesh::GenerateTorus(float a_fOuterRadius, float a_fInnerRadius, int a_nSu
 		verticies.push_back(vertex);
 	}
 
-	//Create List of Quad Verticies
-	std::vector<vector3> quadVerticies;
 	//Create Circles Around Ring
 	for (int i = 0; i < a_nSubdivisionsB; i++)
 	{
-		//Create List of Circle Verticies
-		std::vector<vector3> circleVerticies = verticies;
+		//Set List of Circle Verticies
+		circleVerticies = verticies;
 	
 		//Create Circle Transform Matrix
 		matrix4 m4Transform;
@@ -274,23 +276,49 @@ void MyMesh::GenerateTorus(float a_fOuterRadius, float a_fInnerRadius, int a_nSu
 		m4Transform = glm::translate(m4Transform, vector3(a_fOuterRadius, 0.0f, 0.0f));
 	
 		//Transform Each Vertex
-		for (int j = 0; j < a_nSubdivisionsB; j++)
+		for (int i = 0; i < a_nSubdivisionsB; i++)
 		{
-			circleVerticies[j] = m4Transform * vector4(circleVerticies[j], 1.0f);
-			quadVerticies.push_back(circleVerticies[j]);
+			circleVerticies[i] = m4Transform * vector4(circleVerticies[i], 1.0f);
 		}
 	
 		//Set Center of Circles
 		vector3 center = ZERO_V3;
 		center = m4Transform * vector4(center, 1.0f);
+		//Create Circles
+		for (int i = 0; i < a_nSubdivisionsA; i++)
+		{
+			AddTri(center,
+				circleVerticies[i],
+				circleVerticies[(i + 1) % a_nSubdivisionsB]);
+		}
 	}
-	//Create Torus
-	for (int j = 0; j < a_nSubdivisionsA; j++)
+
+	//Create Ring Connections
+	for (int i = 0; i < a_nSubdivisionsB; i++)
 	{
-		AddQuad(quadVerticies[j],
-			quadVerticies[(j + 1) % a_nSubdivisionsB],
-			quadVerticies[(j + 1 + a_nSubdivisionsB) % a_nSubdivisionsB],
-			quadVerticies[(j + a_nSubdivisionsB) % a_nSubdivisionsB]);
+		//Set List of Quad Verticies
+		quadVerticies = verticies;
+
+		//Create Quad Transform Matrix
+		matrix4 m4Transform;
+		//Rotate and Translate Matrix
+		m4Transform = glm::rotate(IDENTITY_M4, angle * i, AXIS_Y);
+		m4Transform = glm::translate(m4Transform, vector3(a_fOuterRadius, 0.0f, 0.0f));
+
+		//Transform Each Vertex
+		for (int i = 0; i < a_nSubdivisionsB; i++)
+		{
+			quadVerticies[i] = m4Transform * vector4(quadVerticies[i], 1.0f);
+		}
+
+		//Create Quads
+		for (int i = 0; i < a_nSubdivisionsA; i++)
+		{
+			AddQuad(quadVerticies[i],
+				quadVerticies[(i + 1) % a_nSubdivisionsB],
+				quadVerticies[(i + 2) % a_nSubdivisionsB] * vector3(circleVerticies[i].x * angle, circleVerticies[i].y * angle, circleVerticies[i].z * angle),
+				quadVerticies[(i + 3) % a_nSubdivisionsB] * vector3(circleVerticies[i].x * angle, circleVerticies[i].y * angle, circleVerticies[i].z * angle));
+		}
 	}
 	// -------------------------------
 
