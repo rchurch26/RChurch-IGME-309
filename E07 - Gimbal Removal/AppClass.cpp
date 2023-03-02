c6425eac5ebfdd9427b277fe2464a3b8f293057a
Rahmel Church Jr <rac3090@rit.edu>
2023-03-02 10:48:24 -0500

    Looked at Projections

--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -38,12 +38,16 @@ void Application::Display(void)
 	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
 	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
 
-	quaternion qModelx = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	//quaternion qModelx = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	quaternion q1 = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	quaternion q2 = glm::angleAxis(glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
+	quaternion q3 = glm::angleAxis(glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
+	quaternion q4;
 	/*
 	* The following line was replaced by the model manager so we can see a model instead of a cone
 	*/
 	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
-	m_pModelMngr->AddModelToRenderList(m_sSteve, ToMatrix4(qModelx));
+	m_pModelMngr->AddModelToRenderList(m_sSteve, ToMatrix4(q4));
 
 
 	// draw a skybox
