266b74267277765fdc25d96db3435955223f2c93
rchurch26 <rahmelchurch@gmail.com>
2023-03-05 18:41:28 -0500

    Halfway Completed E05 & 7

--- E05 - Transformations/AppClass.cpp
+++ E05 - Transformations/AppClass.cpp
@@ -42,55 +42,83 @@ void Application::Display(void)
 	//Calculate the model, view and projection matrix
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+	//Get Delta Time
+	float time = 0.0f;
+	uint clock = m_pSystem->GenClock();
+	time += m_pSystem->GetDeltaTime(clock);
+	//Handle Moving Meshes
+	uint j = 0;
+	float originalX = -5.0f;
+	vector3 position(originalX, 0.0f, 0.0f);
+	position.x = originalX + time;
+	matrix4 m4Position = glm::translate(position);
 	//Create Top Antennas
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-3.0f, 3.75f, 0.0f)));
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(3.0f, 3.75f, 0.0f)));
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-3.0f, 3.75f, 0.0f)));
+	j++;
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(3.0f, 3.75f, 0.0f)));
+	j++;
 	//Create Bottom Antennas
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-2.0f, 2.75f, 0.0f)));
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(2.0f, 2.75f, 0.0f)));
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-2.0f, 2.75f, 0.0f)));
+	j++;
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(2.0f, 2.75f, 0.0f)));
+	j++;
 	//Top of Head
 	for (uint i = 0; i < borderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-3.0f + i, 1.75f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-3.0f + i, 1.75f, 0.0f)));
+		j++;
 	}
 	//Second Row of Head
 	for (uint i = 0; i < secondRowBorderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-4.0f + i, 0.75f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-4.0f + i, 0.75f, 0.0f)));
+		j++;
 	}
 	for (uint i = 0; i < secondRowMidCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-1.0f + i, 0.75f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-1.0f + i, 0.75f, 0.0f)));
+		j++;
 	}
 	for (uint i = 0; i < secondRowBorderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(3.0f + i, 0.75f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(3.0f + i, 0.75f, 0.0f)));
+		j++;
 	}
 	//Third Row of Head
 	for (uint i = 0; i < thirdRowCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-5.0f + i, -0.25f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-5.0f + i, -0.25f, 0.0f)));
+		j++;
 	}
 	//Fourth Row of Head
-	m_pMesh->Render(m4Projection, m4View, glm::translate(vector3(-5.0f, -1.25f, 0.0f)));
+	m_pMesh->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-5.0f, -1.25f, 0.0f)));
+	j++;
 	for (uint i = 0; i < borderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-3.0f + i, -1.25f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-3.0f + i, -1.25f, 0.0f)));
+		j++;
 	}
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(5.0f, -1.25f, 0.0f)));
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(5.0f, -1.25f, 0.0f)));
+	j++;
 	//Fifth Row of Head
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-5.0f, -2.25f, 0.0f)));
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-3.0f, -2.25f, 0.0f)));
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(3.0f, -2.25f, 0.0f)));
-	meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(5.0f, -2.25f, 0.0f)));
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-5.0f, -2.25f, 0.0f)));
+	j++;
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-3.0f, -2.25f, 0.0f)));
+	j++;
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(3.0f, -2.25f, 0.0f)));
+	j++;
+	meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(5.0f, -2.25f, 0.0f)));
+	j++;
 	//Mouth of Head
 	for (uint i = 0; i < mouthBorderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(-2.0f + i, -3.25f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(-2.0f + i, -3.25f, 0.0f)));
+		j++;
 	}
 	for (uint i = 0; i < mouthBorderCount; i++)
 	{
-		meshes[0]->Render(m4Projection, m4View, glm::translate(vector3(1.0f + i, -3.25f, 0.0f)));
+		meshes[j]->Render(m4Projection, m4View, glm::translate(m4Position, vector3(1.0f + i, -3.25f, 0.0f)));
+		j++;
 	}
 
 	// draw a skybox
@@ -113,8 +141,11 @@ void Application::Release(void)
 	//Release meshes
 	for (uint i = 0; i < meshes.size(); i++)
 	{
+		if (meshes[i] != nullptr)
+		{
 		delete meshes[i];
 		meshes[i] == nullptr;
+		}
 	}
 
 	//release GUI
--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -34,19 +34,14 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
+	m_qOrientation = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	m_qOrientation = glm::rotate(m_qOrientation, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
+	m_qOrientation = glm::rotate(m_qOrientation, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
+	m_m4Model = glm::toMat4(m_qOrientation);
 
-	//quaternion qModelx = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	//quaternion q1 = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	//quaternion q2 = glm::angleAxis(glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	//quaternion q3 = glm::angleAxis(glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	//quaternion q4;
 	/*
 	* The following line was replaced by the model manager so we can see a model instead of a cone
 	*/
-	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
 	m_pModelMngr->AddModelToRenderList(m_sSteve, ToMatrix4(m_m4Model));
 
 
