a20dfeffa224958224cec7421f7b8791e226799c
rchurch26 <rahmelchurch@gmail.com>
2023-04-02 23:28:00 -0400

    Gotta fix gimbal lock

--- HW03_Camera/MyCamera.cpp
+++ HW03_Camera/MyCamera.cpp
@@ -47,11 +47,13 @@ void MyCamera::CalculateView(void)
 	//		 it will receive information from the main code on how much these orientations
 	//		 have change so you only need to focus on the directional and positional 
 	//		 vectors. There is no need to calculate any right click process or connections.
+	//Create Quats
 	quaternion xOrientation = glm::angleAxis(m_v3PitchYawRoll.x, AXIS_X);
 	quaternion yOrientation = glm::angleAxis(m_v3PitchYawRoll.y, AXIS_Y);
 	quaternion orientation = xOrientation * yOrientation;
 	//Apply orientation to directional vectors
 	m_v3Rightward = m_v3Rightward * orientation;
+	//m_v3Upward = m_v3Upward * yOrientation;
 	m_v3Forward = m_v3Forward * orientation;
 	m_v3Target = m_v3Position + m_v3Forward;
 	m_m4View = glm::lookAt(m_v3Position, m_v3Target, m_v3Upward);
